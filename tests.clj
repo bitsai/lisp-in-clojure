9bf9bfe17d9a0a52617c8fc53739388dbe77a322
Benny Tsai <benny.tsai@gmail.com>
2010-08-30 23:01:01 -0600

    making tests self-verifying

--- tests.clj
+++ tests.clj
@@ -7,79 +7,109 @@
 	  ["false" false]])
 
 (defn _test
-  ([exp-str] (_test exp-str ""))
-  ([exp-str env-str]
+  ([exp-str answer] (_test exp-str "" answer))
+  ([exp-str env-str answer]
      (let [test-exp (_read exp-str)
-	   test-env (_read env-str)]
-       ;; (println (pm/_eval test-exp (concat test-env env)))
-       (println (pg/_eval test-exp (concat test-env env))))))
+	   test-env (_read env-str)
+	   ;; result (pm/_eval test_exp (concat test-env env))
+	   result (pg/_eval test-exp (concat test-env env))]
+       (cond (= result answer) (println "OK")
+	     :else (println "FAIL")))))
 
-;; quote
-(_test "(quote a)")
+(println "\nquote")
+(_test "(quote a)"
+       "a")
 ;;(_test "'a") ;; needs support for ' macro
-(_test "(quote (a b c))")
+(_test "(quote (a b c))"
+       '("a" "b" "c"))
 
-;; atom
-(_test "(atom (quote a))")
-(_test "(atom (quote (a b c)))")
-(_test "(atom (quote ()))")
-(_test "(atom (atom (quote a)))")
-(_test "(atom (quote (atom (quote a))))")
+(println "\natom")
+(_test "(atom (quote a))"
+       true)
+(_test "(atom (quote (a b c)))"
+       false)
+(_test "(atom (quote ()))"
+       true)
+(_test "(atom (atom (quote a)))"
+       true)
+(_test "(atom (quote (atom (quote a))))"
+       false)
 
-;; eq
-(_test "(eq (quote a) (quote a))")
-(_test "(eq (quote a) (quote b))")
-(_test "(eq (quote ()) (quote ()))")
+(println "\neq")
+(_test "(eq (quote a) (quote a))"
+       true)
+(_test "(eq (quote a) (quote b))"
+       false)
+(_test "(eq (quote ()) (quote ()))"
+       true)
 
-;; car
-(_test "(car (quote (a b c)))")
+(println "\ncar")
+(_test "(car (quote (a b c)))"
+       "a")
 
-;; cdr
-(_test "(cdr (quote (a b c)))")
+(println "\ncdr")
+(_test "(cdr (quote (a b c)))"
+       '("b" "c"))
 
-;; cons
-(_test "(cons (quote a) (quote (b c)))")
-(_test "(cons (quote a) (cons (quote b) (cons (quote c) (quote ()))))")
-(_test "(car (cons (quote a) (quote (b c))))")
-(_test "(cdr (cons (quote a) (quote (b c))))")
+(println "\ncons")
+(_test "(cons (quote a) (quote (b c)))"
+       '("a" "b" "c"))
+(_test "(cons (quote a) (cons (quote b) (cons (quote c) (quote ()))))"
+       '("a" "b" "c"))
+(_test "(car (cons (quote a) (quote (b c))))"
+       "a")
+(_test "(cdr (cons (quote a) (quote (b c))))"
+       '("b" "c"))
 
-;; cond
+(println "\ncond")
 (_test (str "(cond ((eq (quote a) (quote b)) (quote first))"
-	    "      ((atom (quote a)) (quote second)))"))
+	    "      ((atom (quote a)) (quote second)))")
+       "second")
 
-;; lambda
-(_test "((lambda (x) (cons x (quote (b)))) (quote a))")
+(println "\nlambda")
+(_test "((lambda (x) (cons x (quote (b)))) (quote a))"
+       '("a" "b"))
 (_test (str "((lambda (x y) (cons x (cdr y)))"
 	    " (quote z)"
-	    " (quote (a b c)))"))
+	    " (quote (a b c)))")
+       '("z" "b" "c"))
 (_test (str "((lambda (f) (f (quote (b c))))"
-	    " (quote (lambda (x) (cons (quote a) x))))"))
+	    " (quote (lambda (x) (cons (quote a) x))))")
+       '("a" "b" "c"))
 
-;; label
+(println "\nlabel")
 (_test "(subst (quote m) (quote b) (quote (a b (a b c) d)))"
        (str "((subst (label subst (lambda (x y z)"
 	    "                       (cond ((atom z)"
 	    "                              (cond ((eq z y) x)"
 	    "                                    (true z)))"
 	    "                             (true (cons (subst x y (car z))"
-	    "                                         (subst x y (cdr z)))))))))"))
+	    "                                         (subst x y (cdr z)))))))))")
+       '("a" "m" ("a" "m" "c") "d"))
 
-;; eval
+(println "\neval")
 (_test "x"
-       "((x a) (y b))")
-(_test "(eq (quote a) (quote a))")
+       "((x a) (y b))"
+       "a")
+(_test "(eq (quote a) (quote a))"
+       true)
 (_test "(cons x (quote (b c)))"
-       "((x a) (y b))")
+       "((x a) (y b))"
+       '("a" "b" "c"))
 (_test (str "(cond ((atom x) (quote atom))"
 	    "      (true (quote list)))")
-       "((x (quote (a b))))")
+       "((x (quote (a b))))"
+       "list")
 (_test "(f (quote (b c)))"
-       "((f (lambda (x) (cons (quote a) x))))")
+       "((f (lambda (x) (cons (quote a) x))))"
+       '("a" "b" "c"))
 (_test (str "((label firstatom (lambda (x)"
 	    "                    (cond ((atom x) x)"
 	    "                          (true (firstatom (car x))))))"
 	    " y)")
-       "((y ((a b) (c d))))")
+       "((y ((a b) (c d))))"
+       "a")
 (_test (str "((lambda (x y) (cons x (cdr y)))"
 	    " (quote a)"
-	    " (quote (b c d)))"))
+	    " (quote (b c d)))")
+       '("a" "c" "d"))
