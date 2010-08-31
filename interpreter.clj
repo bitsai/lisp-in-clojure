46acfdc30be5e2bb6787e97f70c178268daf0c2c
Benny Tsai <benny.tsai@gmail.com>
2010-08-31 12:17:26 -0600

    streamline interpreter

--- interpreter.clj
+++ interpreter.clj
@@ -1,6 +1,6 @@
-(ns eval-pg)
+(ns interpreter)
 
-;; primitives
+;; primitive
 (defn _atom [x]
   (cond
    (string? x) true
@@ -8,42 +8,19 @@
    (false? x) true
    (empty? x) true
    :else false))
-(defn _eq [x y] (= x y))
 (defn _car [x] (first x))
-(defn _cdr [x] (rest x))
-(defn _cons [x y] (cons x y))
+(defn _cdr [x] (next x))
 
 ;; derived
-(defn _caar [x] (_car (_car x)))
-(defn _cadr [x] (_car (_cdr x)))
-(defn _cadar [x] (_car (_cdr (_car x))))
-(defn _caddr [x] (_car (_cdr (_cdr x))))
-(defn _caddar [x] (_car (_cdr (_cdr (_car x)))))
-(defn _list [x y] (_cons x (_cons y [])))
-(defn _null [x] (_eq x []))
-(defn _and [x y]
-  (cond
-   x (cond
-      y true
-      :else false)
-   :else false))
-(defn _not [x]
-  (cond
-   x false
-   :else true))
-(defn _append [x y]
-  (cond
-   (_null x) y
-   :else (_cons (_car x) (_append (_cdr x) y))))
-(defn _pair [x y]
-  (cond
-   (_and (_null x) (_null y)) []
-   (_and (_not (_atom x)) (_not (_atom y))) (_cons (_list (_car x) (_car y))
-						   (_pair (_cdr x) (_cdr y)))))
+(defn _caar [x] (ffirst x))
+(defn _cadr [x] (fnext x))
+(defn _cadar [x] (fnext (first x)))
+(defn _caddr [x] (fnext (next x)))
+(defn _caddar [x] (fnext (nfirst x)))
+(defn _pair [x y] (map list x y))
 (defn _assoc [x y]
-  (cond
-   (_eq (_caar y) x) (_cadar y)
-   :else (_assoc x (_cdr y))))
+  (let [match (first (filter #(= x (first %)) y))]
+    (second match)))
 
 ;; eval and friends
 (declare _eval _evcon _evlis)
@@ -52,23 +29,23 @@
   (cond
    (_atom e) (_assoc e a)
    (_atom (_car e)) (cond
-		     (_eq (_car e) "quote") (_cadr e)
-		     (_eq (_car e) "atom") (_atom (_eval (_cadr e) a))
-		     (_eq (_car e) "eq") (_eq (_eval (_cadr e) a)
-					      (_eval (_caddr e) a))
-		     (_eq (_car e) "car") (_car (_eval (_cadr e) a))
-		     (_eq (_car e) "cdr") (_cdr (_eval (_cadr e) a))
-		     (_eq (_car e) "cons") (_cons (_eval (_cadr e) a)
-						  (_eval (_caddr e) a))
-		     (_eq (_car e) "cond") (_evcon (_cdr e) a)
-		     :else (_eval (_cons (_assoc (_car e) a)
-					 (_cdr e))
+		     (= (_car e) "quote") (_cadr e)
+		     (= (_car e) "atom") (_atom (_eval (_cadr e) a))
+		     (= (_car e) "eq") (= (_eval (_cadr e) a)
+					  (_eval (_caddr e) a))
+		     (= (_car e) "car") (_car (_eval (_cadr e) a))
+		     (= (_car e) "cdr") (_cdr (_eval (_cadr e) a))
+		     (= (_car e) "cons") (cons (_eval (_cadr e) a)
+					       (_eval (_caddr e) a))
+		     (= (_car e) "cond") (_evcon (_cdr e) a)
+		     :else (_eval (cons (_assoc (_car e) a)
+					(_cdr e))
 				  a))
-   (_eq (_caar e) "label") (_eval (_cons (_caddar e) (_cdr e))
-				  (_cons (_list (_cadar e) (_car e)) a))
-   (_eq (_caar e) "lambda") (_eval (_caddar e)
-				   (_append (_pair (_cadar e) (_evlis (_cdr e) a))
-					    a))))
+   (= (_caar e) "label") (_eval (cons (_caddar e) (_cdr e))
+				(cons (list (_cadar e) (_car e)) a))
+   (= (_caar e) "lambda") (_eval (_caddar e)
+				 (concat (_pair (_cadar e) (_evlis (_cdr e) a))
+					 a))))
 
 (defn _evcon [c a]
   (cond
@@ -77,6 +54,6 @@
 
 (defn _evlis [m a]
   (cond
-   (_null m) []
-   :else (_cons (_eval (_car m) a)
-		(_evlis (_cdr m) a))))
+   (empty? m) '()
+   :else (cons (_eval (_car m) a)
+	       (_evlis (_cdr m) a))))
