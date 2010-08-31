5e39d2a1bf5b670ed3fe8860c88c086ce9580bef
Benny Tsai <benny.tsai@gmail.com>
2010-08-31 12:28:13 -0600

    update var and fn names for consistency

--- reader.clj
+++ reader.clj
@@ -1,23 +1,23 @@
 (ns reader
   (:require [clojure.string :as str]))
 
-(defn tokenize [line]
+(defn _tokenize [line]
   (let [tokens (-> line
 		   (str/replace "(" " ( ")
 		   (str/replace ")" " ) ")
 		   (str/split #"\s"))]
     (filter (comp not empty?) tokens)))
 
-(defn vectorize
-  ([tokens] (vectorize '() tokens))
+(defn _parse
+  ([tokens] (_parse '() tokens))
   ([acc tokens]
      (let [head (first tokens)
 	   tail (rest tokens)]
        (cond
 	(empty? tokens) (first acc)
 	(= head ")") [acc tail]
-	(= head "(") (let [[v new-tokens] (vectorize tail)]
-		       (vectorize (concat acc (list v)) new-tokens))
-	:else (vectorize (concat acc (list head)) tail)))))
+	(= head "(") (let [[sub-acc new-tokens] (_parse tail)]
+		       (_parse (concat acc (list sub-acc)) new-tokens))
+	:else (_parse (concat acc (list head)) tail)))))
 
-(defn _read [line] (vectorize (tokenize line)))
+(defn _read [line] (_parse (_tokenize line)))
--- repl.clj
+++ repl.clj
@@ -1,13 +1,13 @@
 (ns repl
   (:use [interpreter :only (_eval)])
-  (:use [reader :only (tokenize _read)]))
+  (:use [reader :only (_tokenize _read)]))
 
 (def env [["true" true]
 	  ["false" false]])
 
-(defn repl []
+(defn _repl []
   (let [exp-str (read-line)
-	tokens (tokenize exp-str)]
+	tokens (_tokenize exp-str)]
     (if (not= (first tokens) "exit")
       (let [exp (_read exp-str)
 	    result (_eval exp env)]
@@ -16,4 +16,4 @@
 
 (println "REPL started!")
 
-(repl)
+(_repl)
