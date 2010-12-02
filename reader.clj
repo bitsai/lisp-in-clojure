e3bd72a541a8f84f161b474df9544d8d37b724c7
Benny Tsai <benny.tsai@gmail.com>
2010-12-02 00:19:50 -0700

    refactoring for readability

--- reader.clj
+++ reader.clj
@@ -7,23 +7,21 @@
 		   (str/replace ")" " ) ")
 		   (str/replace "'" " ' ")
 		   (str/split #"\s"))]
-    (filter (comp not empty?) tokens)))
+    (remove empty? tokens)))
 
 (defn listify
-  ([tokens] (first (second (listify tokens nil))))
-  ([tokens lst]
-     (let [head (first tokens)
-	   tail (rest tokens)]
-       (cond
-	(empty? tokens) [nil lst]
-	(= head "(") (let [[new-tokens sub-lst] (listify tail nil)
-			   new-lst (concat lst (list sub-lst))]
-		       (listify new-tokens new-lst))
-	(= head ")") [tail lst]
-	(= head "'") (let [[new-tokens sub-lst] (listify tail nil)
-			   q (list "quote" (first sub-lst))
-			   new-lst (concat lst (list q) (rest sub-lst))]
-		       [new-tokens new-lst])
-	:else (listify tail (concat lst (list head)))))))
+  ([tokens] (ffirst (listify '() tokens)))
+  ([acc [x & xs :as tokens]]
+     (cond
+      (empty? tokens) [acc nil]
+      (= x "(") (let [[sub-list new-tokens] (listify nil xs)
+		      new-acc (concat acc (list sub-list))]
+		  (listify new-acc new-tokens))
+      (= x ")") [acc xs]
+      (= x "'") (let [[sub-list new-tokens] (listify nil xs)
+		      quoted (list "quote" (first sub-list))
+		      new-acc (concat acc (list quoted) (rest sub-list))]
+		  [new-acc new-tokens])
+      :else (listify (concat acc (list x)) xs))))
 
 (defn read* [exp] (listify (tokenize exp)))
