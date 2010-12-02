b23ba8fe5dc77aef001d9a22ba4e0b50a93815a6
Benny Tsai <benny.tsai@gmail.com>
2010-12-02 02:00:34 -0700

    re-wrote tests

--- tests.clj
+++ tests.clj
@@ -1,159 +1,158 @@
 (ns tests
+  (:use [clojure.test :only (deftest are run-tests)])
   (:use [eval :only (env eval*)])
   (:use [reader :only (read*)]))
 
-(defn run-test [exp answer]
-  (let [result (eval* (read* exp) env)]
-    (if (= result answer) (println "OK")
-	(println "FAIL"))))
+(deftest tests
+  (are [exp result] (= (eval* (read* exp) env) result)
 
-(println "\n(QUOTE)")
-(run-test "(quote a)"
-	  "a")
-(run-test "'a",
-	  "a")
-(run-test "(quote (a b c))"
-	  ["a" "b" "c"])
+       "(quote a)"
+       "a"
 
-(println "\n(ATOM)")
-(run-test "(atom 'a)"
-	  "t")
-(run-test "(atom '(a b c))"
-	  "f")
-(run-test "(atom '())"
-	  "t")
-(run-test "(atom (atom 'a))"
-	  "t")
-(run-test "(atom '(atom 'a))"
-	  "f")
+       "'a"
+       "a"
 
-(println "\n(EQ)")
-(run-test "(eq 'a 'a)"
-	  "t")
-(run-test "(eq 'a 'b)"
-	  "f")
-(run-test "(eq '() '())"
-	  "t")
+       "(quote (a b c))"
+       ["a" "b" "c"]
 
-(println "\n(CAR)")
-(run-test "(car '(a b c))"
-	  "a")
+       "(atom 'a)"
+       "t"
 
-(println "\n(CDR)")
-(run-test "(cdr '(a b c))"
-	  ["b" "c"])
+       "(atom '(a b c))"
+       "f"
 
-(println "\n(CONS)")
-(run-test "(cons 'a '(b c))"
-	  ["a" "b" "c"])
-(run-test "(cons 'a (cons 'b (cons 'c '())))"
-	  ["a" "b" "c"])
-(run-test "(car (cons 'a '(b c)))"
-	  "a")
-(run-test "(cdr (cons 'a '(b c)))"
-	  ["b" "c"])
+       "(atom '())"
+       "t"
 
-(println "\n(COND)")
-(run-test (str "(cond ((eq 'a 'b) 'first)"
-	       "      ((atom 'a)  'second))")
-	  "second")
+       "(atom (atom 'a))"
+       "t"
 
-(println "\n(LAMBDA)")
-(run-test "((lambda (x) (cons x '(b))) 'a)"
-	  ["a" "b"])
-(run-test (str "((lambda (x y) (cons x (cdr y)))"
-	       " 'z"
-	       " '(a b c))")
-	  ["z" "b" "c"])
-(run-test (str "((lambda (f) (f '(b c)))"
-	       " '(lambda (x) (cons 'a x)))")
-	  ["a" "b" "c"])
+       "(atom '(atom 'a))"
+       "f"
 
-(println "\n(DEFUN SUBST)")
-(run-test "(subst 'm 'b '(a b (a b c) d))"
-	  ["a" "m" ["a" "m" "c"] "d"])
+       "(eq 'a 'a)"
+       "t"
 
-(println "\n(DEFUN CAAR)")
-(run-test "(caar '((a b x) (c d) e))"
-	  "a")
+       "(eq 'a 'b)"
+       "f"
 
-(println "\n(DEFUN CADR)")
-(run-test "(cadr '((a b x) (c d) e))"
-	  ["c" "d"])
+       "(eq '() '())"
+       "t"
 
-(println "\n(DEFUN CADAR)")
-(run-test "(cadar '((a b x) (c d) e))"
-	  "b")
+       "(car '(a b c))"
+       "a"
 
-(println "\n(DEFUN CADDR)")
-(run-test "(caddr '((a b x) (c d) e))"
-	  "e")
+       "(cdr '(a b c))"
+       ["b" "c"]
 
-(println "\n(DEFUN CADDAR)")
-(run-test "(caddar '((a b x) (c d) e))"
-	  "x")
+       "(cons 'a '(b c))"
+       ["a" "b" "c"]
 
-(println "\n(DEFUN TUPLE)")
-(run-test "(tuple 'a 'b)"
-	  ["a" "b"])
+       "(cons 'a (cons 'b (cons 'c '())))"
+       ["a" "b" "c"]
 
-(println "\n(DEFUN NULL)")
-(run-test "(null 'a)"
-	  "f")
-(run-test "(null '())"
-	  "t")
+       "(car (cons 'a '(b c)))"
+       "a"
 
-(println "\n(DEFUN AND)")
-(run-test "(and (atom 'a) (eq 'a 'a))"
-	  "t")
-(run-test "(and (atom 'a) (eq 'a 'b))"
-	  "f")
+       "(cdr (cons 'a '(b c)))"
+       ["b" "c"]
 
-(println "\n(DEFUN NOT)")
-(run-test "(not (eq 'a 'a))"
-	  "f")
-(run-test "(not (eq 'a 'b))"
-	  "t")
+       "(cond ((eq 'a 'b) 'first) ((atom 'a) 'second))"
+       "second"
+	    
+       "((lambda (x) (cons x '(b))) 'a)"
+       ["a" "b"]
 
-(println "\n(DEFUN APPEND)")
-(run-test "(append '(a b) '(c d))"
-	  ["a" "b" "c" "d"])
-(run-test "(append '() '(c d))"
-	  ["c" "d"])
+       "((lambda (x y) (cons x (cdr y))) 'z '(a b c))"
+       ["z" "b" "c"]
 
-(println "\n(DEFUN PAIR)")
-(run-test "(pair '(x y z) '(a b c))"
-	  [["x" "a"] ["y" "b"] ["z" "c"]])
+       "((lambda (f) (f '(b c))) '(lambda (x) (cons 'a x)))"
+       ["a" "b" "c"]
 
-(println "\n(DEFUN ASSOC)")
-(run-test "(assoc 'x '((x a) (y b)))"
-	  "a")
-(run-test "(assoc 'x '((x new) (x a) (y b)))"
-	  "new")
+       "(subst 'm 'b '(a b (a b c) d))"
+       ["a" "m" ["a" "m" "c"] "d"]
 
-(println "\n(DEFUN EVAL)")
-(run-test "(eval 'x '((x a) (y b)))"
-	  "a")
-(run-test "(eval '(eq 'a 'a) '())"
-	  "t")
-(run-test (str "(eval '(cons x '(b c))"
-	       "      '((x a) (y b)))")
-	  ["a" "b" "c"])
-(run-test (str "(eval '(cond ((atom x) 'atom)"
-	       "             ('t 'list))"
-	       "      '((x '(a b))))")
-	  "list")
-(run-test (str "(eval '(f '(b c))"
-	       "      '((f (lambda (x) (cons 'a x)))))")
-	  ["a" "b" "c"])
-(run-test (str "(eval '((label first (lambda (x)"
-	       "                       (cond ((atom x) x)"
-	       "                             ('t (first (car x))))))"
-	       "        y)"
-	       "      '((y ((a b) (c d)))))")
-	  "a")
-(run-test (str "(eval '((lambda (x y) (cons x (cdr y)))"
-	       "        'a"
-	       "        '(b c d))"
-	       "      '())")
-	  ["a" "c" "d"])
+       "(caar '((a b x) (c d) e))"
+       "a"
+
+       "(cadr '((a b x) (c d) e))"
+       ["c" "d"]
+
+       "(cadar '((a b x) (c d) e))"
+       "b"
+
+       "(caddr '((a b x) (c d) e))"
+       "e"
+
+       "(caddar '((a b x) (c d) e))"
+       "x"
+
+       "(tuple 'a 'b)"
+       ["a" "b"]
+
+       "(null 'a)"
+       "f"
+
+       "(null '())"
+       "t"
+
+       "(and (atom 'a) (eq 'a 'a))"
+       "t"
+
+       "(and (atom 'a) (eq 'a 'b))"
+       "f"
+
+       "(not (eq 'a 'a))"
+       "f"
+
+       "(not (eq 'a 'b))"
+       "t"
+
+       "(append '(a b) '(c d))"
+       ["a" "b" "c" "d"]
+
+       "(append '() '(c d))"
+       ["c" "d"]
+
+       "(pair '(x y z) '(a b c))"
+       [["x" "a"] ["y" "b"] ["z" "c"]]
+
+       "(assoc 'x '((x a) (y b)))"
+       "a"
+       
+       "(assoc 'x '((x new) (x a) (y b)))"
+       "new"
+
+       (str "(eval 'x"
+	    "      '((x a) (y b)))")
+       "a"
+
+       (str "(eval '(eq 'a 'a)"
+	    "      '())")
+       "t"
+
+       (str "(eval '(cons x '(b c))"
+	    "      '((x a) (y b)))")
+       ["a" "b" "c"]
+       
+       (str "(eval '(cond ((atom x) 'atom) ('t 'list))"
+	    "      '((x '(a b))))")
+       "list"
+       
+       (str "(eval '(f '(b c))"
+	    "      '((f (lambda (x) (cons 'a x)))))")
+       ["a" "b" "c"]
+       
+       (str "(eval '((label first (lambda (x) (cond ((atom x) x)"
+	    "                                       ('t (first (car x))))))"
+	    "        y)"
+	    "      '((y ((a b) (c d)))))")
+       "a"
+       
+       (str "(eval '((lambda (x y) (cons x (cdr y)))"
+	    "        'a"
+	    "        '(b c d))"
+	    "      '())")
+       ["a" "c" "d"]))
+
+(run-tests)
