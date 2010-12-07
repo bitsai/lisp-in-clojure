2e07d824a3de05b4adcffc02145ed3cd1c84cf53
Benny Tsai <benny.tsai@gmail.com>
2010-12-07 11:21:29 -0700

    fixed 'assoc*'

--- eval.clj
+++ eval.clj
@@ -10,8 +10,6 @@
   (if (= x y)
     "t"
     "f"))
-(defn car [x] (first x))
-(defn cdr [x] (rest x))
 
 ;; derived
 (defn caar [x] (first (first x)))
@@ -22,7 +20,7 @@
 (defn cadddr [x] (first (rest (rest (rest x)))))
 (defn pair [x y] (map list x y))
 (defn assoc* [x y]
-  (if-let [matches (for [[a b] @y :when (= x a)] b)]
+  (if-let [matches (seq (for [[a b] @y :when (= x a)] b))]
     (first matches)
     (throw (Exception. (str x " not defined!")))))
 
@@ -33,45 +31,45 @@
   (try
     (cond
      (= "t" (atom* e)) (assoc* e a)
-     (= "t" (atom* (car e)))
+     (= "t" (atom* (first e)))
      (cond
-      (= (car e) "quote") (cadr e)
-      (= (car e) "atom")  (atom* (eval* (cadr e) a))
-      (= (car e) "eq")    (eq    (eval* (cadr e) a)
-				 (eval* (caddr e) a))
-      (= (car e) "car")   (car   (eval* (cadr e) a))
-      (= (car e) "cdr")   (cdr   (eval* (cadr e) a))
-      (= (car e) "cons")  (cons  (eval* (cadr e) a)
-				 (eval* (caddr e) a)) 
-      (= (car e) "cond")  (evcon (cdr e) a)
-      (= (car e) "defun") (defun e a)
-      :else (eval* (cons (assoc* (car e) a)
-			 (cdr e))
+      (= (first e) "quote") (cadr e)
+      (= (first e) "atom")  (atom* (eval* (cadr e) a))
+      (= (first e) "eq")    (eq    (eval* (cadr e) a)
+				   (eval* (caddr e) a))
+      (= (first e) "car")   (first (eval* (cadr e) a))
+      (= (first e) "cdr")   (rest  (eval* (cadr e) a))
+      (= (first e) "cons")  (cons  (eval* (cadr e) a)
+				   (eval* (caddr e) a))
+      (= (first e) "cond")  (evcon (rest e) a)
+      (= (first e) "defun") (defun e a)
+      :else (eval* (cons (assoc* (first e) a)
+			 (rest e))
 		   a))
      (= (caar e) "label")
-     (eval* (cons (caddar e) (cdr e))
-	    (atom (cons (list (cadar e) (car e))
-			@a)))
+     (eval* (cons (caddar e) (rest e))
+	    (atom (cons (list (cadar e) (first e)) @a)))
      (= (caar e) "lambda")
      (eval* (caddar e)
-	    (atom (concat (pair (cadar e) (evlis (cdr e) a))
+	    (atom (concat (pair (cadar e) (evlis (rest e) a))
 			  @a))))
     (catch Exception ex (.getMessage ex))))
 
 (defn evcon [c a]
   (cond (= "t" (eval* (caar c) a)) (eval* (cadar c) a)
-	:else (evcon (cdr c) a)))
+	:else (evcon (rest c) a)))
 
 (defn evlis [m a]
   (cond (empty? m) nil
-	:else (cons (eval* (car m) a)
-		    (evlis (cdr m) a))))
+	:else (cons (eval* (first m) a)
+		    (evlis (rest m) a))))
 
 (defn defun [e a]
   (let [name (cadr e)
 	args (caddr e)
-	body (cadddr e)]
-    (swap! a conj [name ["label" name ["lambda" args body]]])))
+	body (cadddr e)
+	new-pair [name ["label" name ["lambda" args body]]]]
+    (swap! a conj new-pair)))
 
 ;; environment
 (def env (atom []))
