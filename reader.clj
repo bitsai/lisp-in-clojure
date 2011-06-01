51f56f0d29df7e261e334ebf44b8e1053908e594
Benny Tsai <benny.tsai@gmail.com>
2011-06-01 10:47:06 -0600

    replace cond with case in reader code

--- reader.clj
+++ reader.clj
@@ -11,19 +11,19 @@
 (declare micro-read read-list)
 
 (defn micro-read [[t & ts]]
-  (cond
-   (= t "(") (read-list '() ts)
-   (= t "'") (let [[new-t new-ts] (micro-read ts)]
-	       [(list "quote" new-t) new-ts])
-   :else [t ts]))
+  (case t
+        "(" (read-list '() ts)
+        "'" (let [[new-t new-ts] (micro-read ts)]
+              [(list "quote" new-t) new-ts])
+        [t ts]))
 
 (defn read-list [list-so-far tokens]
   (let [[t ts] (micro-read tokens)]
-    (cond
-     (= t ")") [(reverse list-so-far) ts]
-     (= t "(") (let [[new-list new-tokens] (read-list '() ts)]
-		 (read-list (conj list-so-far new-list) new-tokens))
-     :else (read-list (conj list-so-far t) ts))))
+    (case t
+          ")" [(reverse list-so-far) ts]
+          "(" (let [[new-list new-tokens] (read-list '() ts)]
+                (read-list (conj list-so-far new-list) new-tokens))
+          (read-list (conj list-so-far t) ts))))
 
 (defn read* [exp]
   (first (micro-read (tokenize exp))))
